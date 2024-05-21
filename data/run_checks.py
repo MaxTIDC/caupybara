aa0246518ed2f2e335d03e1af0346198922b02c3
mm2320 <mm2320@ic.ac.uk>
2024-05-21 12:39:18 +0100

    Add support for building binaries

--- data/run_checks.py
+++ data/run_checks.py
@@ -3,32 +3,39 @@ import os
 import subprocess
 import sys
 
-# python run_checks.py '..'
-
 def run_causality_checks(data: dict, project_dir: str, causality: str) -> dict:
     """
-    Run causality check JAR file, with provided input arguments.
+    Run causality check executable file, with provided input arguments.
     """
     output = dict()
 
     JAR_PATH = os.path.join(project_dir, "fyp-causality.jar")
+    BIN_PATH = os.path.join(project_dir, "bin", "fyp-causality.exe")  # TODO: support for Linux
     TRACE_DIR = os.path.join(project_dir, "input-files/")
 
     for trace in data.keys():  # For each trace file
         output.update({trace : dict()})
         for ltl in data.get(trace).keys():  # For each LTL property
             try:
+#                 args = [
+#                     'java', '-jar', JAR_PATH,
+#                     "-c", causality,
+#                     "-o", "pickled",
+#                     "-l", ltl,
+#                     "-t", TRACE_DIR + trace,
+#                 ]
                 args = [
-                    'java', '-jar', JAR_PATH, 
+                    BIN_PATH,
                     "-c", causality,
                     "-o", "pickled",
                     "-l", ltl,
                     "-t", TRACE_DIR + trace,
                 ]
-                process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # TODO: fix
+                process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # TODO: jar / binary mode
                 
                 stdout, stderr = process.communicate()
-                # print((stdout, stderr))
+                if stderr:
+                    print("ERROR: " + stderr)
                 output.get(trace).update({ltl : json.loads(stdout.decode('utf-8').strip())})
 
             except Exception as e:
@@ -38,7 +45,7 @@ def run_causality_checks(data: dict, project_dir: str, causality: str) -> dict:
 
 def write_to_file(json_file: str, output: dict) -> None:
     """
-    Write structured outputs of JAR execution to JSON files.
+    Write output dictionary to JSON files.
     """
     try:
         with open(json_file, 'w') as f:
@@ -57,9 +64,9 @@ if __name__ == "__main__":
         print(f"'{project_dir}' is not a valid directiory!")
     else:
         # Read input JSON
-        input_json = "./input.json"
-        output_json_beer = "./beer2011.json"
-        output_json_meng = "./meng2024.json"
+        input_json = os.path.join(project_dir, "data", "input.json")
+        output_json_beer = os.path.join(project_dir, "data", "beer2011.json")
+        output_json_meng = os.path.join(project_dir, "data", "meng2024.json")
 
         with open(input_json, 'r') as f:
             data = json.load(f)
